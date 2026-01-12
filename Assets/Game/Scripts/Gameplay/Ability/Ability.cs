75050f0904c4c953e133fa3eed619301945e86c3
EugeneM23 <johnyanimation@gmail.com>
2026-01-12 14:29:36 +0200

    Update

---
+++ Assets/Game/Scripts/Gameplay/Ability/Ability.cs
@@ -0,0 +1,12 @@
+
+using Atomic.Entities;
+
+namespace Game.Gameplay
+{
+    public class Ability : Entity
+    {
+        public Ability(in string name) : base(in name)
+        {
+        }
+    }
+}
